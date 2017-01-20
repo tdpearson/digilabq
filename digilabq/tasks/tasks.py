e0f6aa25d872a591e75180799445d0d08091ce20
Tyler Pearson <tylerdpearson@gmail.com>
2017-01-20 14:46:38 -0600

    seperate image processing logic

--- digilabq/tasks/tasks.py
+++ digilabq/tasks/tasks.py
@@ -16,6 +16,32 @@ hostname ="http://localhost"
 #libtiff-tools needs to be installed within the docker container
 
 
+def _processimage(inpath, outpath, outformat="TIFF", filter="ANTIALIAS", scale=None, crop=None):
+    """
+    Internal function to create image derivatives
+    """
+        
+    try:
+        image = Image.open(inpath)
+    except (IOError, OSError):
+        # workaround for Pillow unrecognized tiff image
+        if inpath.split(".")[-1].upper() in ["TIF", "TIFF"]:
+            with NamedTemporaryFile() as tmpfile:
+                check_call(("tiff2rgba", inpath, tmpfile.name))
+                image = Image.open(tmpfile.name)
+        else:
+            raise Exception
+        
+    if crop:
+        image = image.crop(crop)
+        
+    if scale:
+        imagefilter = getattr(Image, filter.upper())
+        size = [x * scale for x in image.size]
+        image.thumbnail(size, imagefilter)
+    image.save(outpath, outformat)
+
+
 @task()
 def processimage(inpath, outpath, outformat="TIFF", filter="ANTIALIAS", scale=None, crop=None):
     """
@@ -27,32 +53,21 @@ def processimage(inpath, outpath, outformat="TIFF", filter="ANTIALIAS", scale=No
       outformat - string representation of image format - default is "TIFF"
       scale - percentage to scale by represented as a decimal
       filter - string representing filter to apply to resized image - default is "ANTIALIAS"
-      crop - list of coordinates to crop from - i.e. [10,10,200,200]
+      crop - list of coordinates to crop from - i.e. [10, 10, 200, 200]
     """
-
+    
     task_id = str(processimage.request.id)
     #create Result Directory
     resultDir = os.path.join(basedir, 'oulib_tasks/', task_id)
     os.makedirs(resultDir)
 
-    try:
-        image = Image.open(os.path.join(basedir, inpath))
-    except (IOError, OSError):
-        # workaround for Pillow unrecognized tiff image
-        if inpath.split(".")[-1].upper() in ["TIF", "TIFF"]:
-            with NamedTemporaryFile() as tmpfile:
-                check_call(("tiff2rgba", os.path.join(basedir, inpath), tmpfile.name))
-                image = Image.open(tmpfile.name)
-        else:
-            raise Exception
+    _preocessimage( inpath=os.path.join(basedir, inpath),
+                    outpath=os.path.join(basefir, outpath),
+                    outformat=outformat,
+                    filter=filter,
+                    scale=scale,
+                    crop=crop
+                  )
 
-    if crop:
-        image = image.crop(crop)
-
-    if scale:
-        imagefilter = getattr(Image, filter.upper())
-        size = [x * scale for x in image.size]
-        image.thumbnail(size, imagefilter)
-    image.save(os.path.join(resultDir, outpath), outformat)
     return "{0}/oulib_tasks/{1}".format(hostname,task_id)
-    
+
