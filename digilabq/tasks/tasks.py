6224d22711f3c7289a58ca7e81ed66523eee8382
tdpearson <tylerdpearson@gmail.com>
2017-01-25 16:39:38 -0600

    update with imagemagick

--- digilabq/tasks/tasks.py
+++ digilabq/tasks/tasks.py
@@ -1,44 +1,41 @@
 from celery.task import task
-#from celery.task.sets import TaskSet
-#from celery.exceptions import Ignore
-#from celery import states
 from dockertask import docker_task
-from subprocess import check_call
-from tempfile import NamedTemporaryFile
 from PIL import Image
+from subprocess import check_call, check_output
+from tempfile import NamedTemporaryFile
 import os
 
+#Default base directory
+basedir = "/data/static"
+hostname = "http://localhost"
 
-#Default base directory 
-basedir="/data/static/"
-hostname ="http://localhost"
-
-#libtiff-tools needs to be installed within the docker container
+#imagemagick needs to be installed within the docker container
 
 
 def _processimage(inpath, outpath, outformat="TIFF", filter="ANTIALIAS", scale=None, crop=None):
     """
     Internal function to create image derivatives
     """
-        
+
     try:
         image = Image.open(inpath)
     except (IOError, OSError):
-        # workaround for Pillow unrecognized tiff image
-        if inpath.split(".")[-1].upper() in ["TIF", "TIFF"]:
+        # workaround for Pillow not handling 16bit sRGB images
+        if "16-bit sRGB" in check_output(("identify", inpath)):
             with NamedTemporaryFile() as tmpfile:
-                check_call(("tiff2rgba", inpath, tmpfile.name))
+                check_call(("convert", inpath, "-depth", "8", tmpfile.name))
                 image = Image.open(tmpfile.name)
         else:
             raise Exception
-        
+
     if crop:
         image = image.crop(crop)
-        
+
     if scale:
         imagefilter = getattr(Image, filter.upper())
         size = [x * scale for x in image.size]
         image.thumbnail(size, imagefilter)
+
     image.save(outpath, outformat)
 
 
@@ -55,19 +52,18 @@ def processimage(inpath, outpath, outformat="TIFF", filter="ANTIALIAS", scale=No
       filter - string representing filter to apply to resized image - default is "ANTIALIAS"
       crop - list of coordinates to crop from - i.e. [10, 10, 200, 200]
     """
-    
+
     task_id = str(processimage.request.id)
     #create Result Directory
-    resultdir = os.path.join(basedir, 'oulib_tasks/', task_id)
-    os.makedirs(resultdir)
+    resultpath = os.path.join(basedir, 'oulib_tasks/', task_id)
+    os.makedirs(resultpath)
 
-    _processimage( inpath=os.path.join(basedir, inpath),
-                   outpath=os.path.join(resultdir, outpath),
-                   outformat=outformat,
-                   filter=filter,
-                   scale=scale,
-                   crop=crop
-                 )
+    _processimage(inpath=os.path.join(basedir, inpath),
+                  outpath=os.path.join(basedir, outpath),
+                  outformat=outformat,
+                  filter=filter,
+                  scale=scale,
+                  crop=crop
+                  )
 
-    return "{0}/oulib_tasks/{1}".format(hostname,task_id)
-
+    return "{0}/oulib_tasks/{1}".format(hostname, task_id)
